4599613f70516bce9b0cc5fe27e9ae31cdf036f1
hussain-sameer-hussain <h__050@hotmail.com>
2025-02-16 20:41:33 +0300

    Update courses (2) (3).sql

--- data/courses (2) (3).sql
+++ data/courses (2) (3).sql
@@ -62,3 +62,127 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 16 فبراير 2025 الساعة 15:52
+-- إصدار الخادم: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `courses`
+--
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `courses`
+--
+
+CREATE TABLE `courses` (
+  `CourseID` varchar(10) NOT NULL,
+  `CourseName` varchar(100) NOT NULL,
+  `Credits` int(11) NOT NULL,
+  `DepartmentID` int(11) DEFAULT NULL,
+  `FacultyID` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `departments`
+--
+
+CREATE TABLE `departments` (
+  `DepartmentID` int(11) NOT NULL,
+  `DepartmentName` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `faculty`
+--
+
+CREATE TABLE `faculty` (
+  `FacultyID` varchar(10) NOT NULL,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `DepartmentID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`CourseID`),
+  ADD KEY `DepartmentID` (`DepartmentID`),
+  ADD KEY `FacultyID` (`FacultyID`);
+
+--
+-- Indexes for table `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`DepartmentID`),
+  ADD UNIQUE KEY `DepartmentName` (`DepartmentName`);
+
+--
+-- Indexes for table `faculty`
+--
+ALTER TABLE `faculty`
+  ADD PRIMARY KEY (`FacultyID`),
+  ADD UNIQUE KEY `Email` (`Email`),
+  ADD KEY `DepartmentID` (`DepartmentID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `departments`
+--
+ALTER TABLE `departments`
+  MODIFY `DepartmentID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- قيود الجداول المُلقاة.
+--
+
+--
+-- قيود الجداول `courses`
+--
+ALTER TABLE `courses`
+  ADD CONSTRAINT `courses_ibfk_1` FOREIGN KEY (`DepartmentID`) REFERENCES `departments` (`DepartmentID`),
+  ADD CONSTRAINT `courses_ibfk_2` FOREIGN KEY (`FacultyID`) REFERENCES `faculty` (`FacultyID`);
+
+--
+-- قيود الجداول `faculty`
+--
+ALTER TABLE `faculty`
+  ADD CONSTRAINT `faculty_ibfk_1` FOREIGN KEY (`DepartmentID`) REFERENCES `departments` (`DepartmentID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
